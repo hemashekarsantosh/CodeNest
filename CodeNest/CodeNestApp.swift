c91a584a1f86557bb97808634c3d72edc7c78dc9
Santosh H <hemashekarsants>
2026-04-20 15:44:36 +0530

    Initial Commit

---
+++ CodeNest/CodeNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeNestApp.swift
+//  CodeNest
+//
+//  Created by Santosh Hemashekar on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
